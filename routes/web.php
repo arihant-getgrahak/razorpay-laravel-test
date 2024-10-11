64c58982050aacfa43a85b243e8c0c3a1b69b177
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-11 21:49:55 +0530

    update web.php route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/pay/verify', [PaymentController::class, 'verify']);
