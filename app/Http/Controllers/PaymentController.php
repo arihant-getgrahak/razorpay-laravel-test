9c6ad33b9f6af4d388ae1d30047e552ac6778531
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-11 21:46:13 +0530

    add func to verify payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,9 +8,15 @@ use Razorpay\Api\Errors\SignatureVerificationError;
 
 class PaymentController extends Controller
 {
-    public function razorpay(Request $request)
+    protected $api;
+    public function __construct()
     {
         $api = new Api(env("RAZORPAY_KEY"), env("RAZORPAY_SECRET"));
+        $this->api = $api;
+    }
+    public function razorpay(Request $request)
+    {
+        $api = $this->api;
         $orderData = $api->order->create([
             'receipt' => '111',
             'amount' => $request->amount * 100,
@@ -23,5 +29,32 @@ class PaymentController extends Controller
             "order_id" => $orderData['id'],
         ];
         return response()->json($data, 200);
+    }
+
+    function verify(Request $request)
+    {
+        $success = true;
+        $error = "Payment Failed!";
+
+        if (empty($request->razorpay_payment_id) === false) {
+            $api = $this->api;
+            try {
+                $attributes = [
+                    'razorpay_order_id' => $request->razorpay_order_id,
+                    'razorpay_payment_id' => $request->razorpay_payment_id,
+                    'razorpay_signature' => $request->razorpay_signature
+                ];
+                $api->utility->verifyPaymentSignature($attributes);
+            } catch (SignatureVerificationError $e) {
+                $success = false;
+                $error = 'Razorpay Error : ' . $e->getMessage();
+            }
+        }
+
+        if ($success === true) {
+            return redirect('/payment')->with('success', $success);
+        } else {
+            return redirect('/payment')->with('error', $error);
+        }
     }
 }
