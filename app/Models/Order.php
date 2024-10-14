c15d7ff4e216f06d2c4a50845ace214b37b10842
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-14 18:23:48 +0530

    feat: add fields in order model class

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'status',
+        'amount',
+        'razorpay_order_id',
+        'razorpay_payment_id',
+        'razorpay_signature',
+    ];
 }
