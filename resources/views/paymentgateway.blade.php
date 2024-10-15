f8e2a7b04614020d2b9cbd704d3e679554ee97d4
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-15 16:22:23 +0530

    feat: update url in app.php and web.php

--- resources/views/paymentgateway.blade.php
+++ resources/views/paymentgateway.blade.php
@@ -1,4 +1,4 @@
-<form name='razorpayform' action="/pay/verify" method="POST">
+<form name='razorpayform' action="arihant/razorpay/public/pay/verify" method="POST">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id">
     <input type="hidden" name="razorpay_signature" id="razorpay_signature">
--- resources/views/razorpay.blade.php
+++ resources/views/razorpay.blade.php
@@ -1,5 +1,5 @@
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
-<form name='razorpayform' action="/pay/verify" method="POST">
+<form name='razorpayform' action="arihant/razorpay/public/pay/verify" method="POST">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id">
     <input type="hidden" name="razorpay_signature" id="razorpay_signature">
