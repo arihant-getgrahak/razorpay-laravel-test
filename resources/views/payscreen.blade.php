cade06e8ab6f55d1caf50d2fe6e650cb24c497b0
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 15:29:38 +0530

    feat: add model to confirm before cancel the order

--- resources/views/payscreen.blade.php
+++ resources/views/payscreen.blade.php
@@ -20,7 +20,40 @@
             Pay Again</button>
     </form>
 
-    <button id="cancelBtn"
+    <div class="hidden fixed inset-0 flex items-center justify-center z-50 backdrop-blur confirm-dialog "
+        id="confirm-dialog">
+        <div class="relative px-4 min-h-screen md:flex md:items-center md:justify-center">
+            <div class=" opacity-25 w-full h-full absolute z-10 inset-0"></div>
+            <div
+                class="bg-white rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg">
+                <div class="md:flex items-center">
+                    <div
+                        class="rounded-full border border-gray-300 flex items-center justify-center w-16 h-16 flex-shrink-0 mx-auto">
+                        <i class="bx bx-error text-3xl">
+                            &#9888;
+                        </i>
+                    </div>
+                    <div class="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
+                        <p class="font-bold">Warning!</p>
+                        <p class="text-sm text-gray-700 mt-1">Do you really want to cancel this order?
+                        </p>
+                    </div>
+                </div>
+                <div class="text-center md:text-right mt-4 md:flex md:justify-end">
+                    <button id="confirm-delete-btn"
+                        class="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-red-200 text-red-700 rounded-lg font-semibold text-sm md:ml-2 md:order-2">
+                        Yes
+                    </button>
+                    <button id="confirm-cancel-btn"
+                        class="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-gray-200 rounded-lg font-semibold text-sm mt-4 md:mt-0 md:order-1">
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <button id="show-modal-btn"
         class="p-2 rounded-md shadow-lg border border-gray-500 hover:bg-blue-500 hover:text-white">Cancel
         Order</button>
 
@@ -168,7 +201,44 @@
             }
 
         }
+    </script>
+    <script>
+        const modal = document.getElementById('confirm-dialog');
+        const showModalBtn = document.getElementById('show-modal-btn');
+        const cancelBtn = document.getElementById('confirm-cancel-btn');
+        const deleteBtn = document.getElementById('confirm-delete-btn');
 
+        showModalBtn.addEventListener('click', () => {
+            console.log('show modal');
+            modal.classList.remove('hidden');
+        });
+
+        cancelBtn.addEventListener('click', () => {
+            modal.classList.add('hidden');
+        });
+
+        deleteBtn.addEventListener('click', async () => {
+            // e.preventDefault();
+            console.log('delete');
+            const urlParams = new URLSearchParams(window.location.search);
+            const order_id = urlParams.get('order_id');
+
+            const res = await fetch("{{ url('api/order/cancel') }}", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    order_id: order_id
+                })
+            })
+            data = await res.json();
+            if (!data.success) {
+                alert(data.message);
+            }
+            window.location.href = 'order-cancel';
+            return;
+        });
     </script>
 </body>
 
